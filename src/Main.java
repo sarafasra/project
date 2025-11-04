564b39d376c7f6d01764369b3e549e7d275dfd77
Hamza <hamzazaidi253@gmail.com>
2025-11-04 17:52:00 +0100

    hhh

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,27 @@
+import java.util.Random;
 import java.util.Scanner;
 
 void main(){
+    //1
     //Scanner input = new Scanner(System.in);
    // System.out.println("Entrer nom ");
     //String nom  = input.next();
     //System.out.println("Bonjour, Sara ! " + nom);
-
+    //2
     //Scanner input = new Scanner(System.in);
    // System.out.println("Entrer un nombre ");
    // int nombre  = input.nextInt();
    // for(int i=0 ;i<=nombre; i++) {
         ;
      //   System.out.println(i);
-
-    Scanner input = new Scanner(System.in);
-    double solde = 1000.00;
-    String chooce ;
+    //4
+    /*Scanner input = new Scanner(System.in);
+    double solde = 2000.00;
+    String choix ;
     while (true) {
         System.out.println("Entrer votre choix : ");
-        chooce = input.next();
-        switch (chooce) {
+        choix = input.next();
+        switch (choix) {
             case "1":
                 System.out.println(solde);
                 break;
@@ -41,8 +43,34 @@ void main(){
             default:
                 System.out.println("Le choix n'est pa exists.");
         }
+    }*/
+
+
+
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int nombreSecret = random.nextInt(10) + 1; // Génère un nombre entre 1 et 10
+        int tentative;
+
+        System.out.println("Devinez le nombre entre 1 et 10 :");
+
+        do {
+            System.out.print("Votre proposition : ");
+            tentative = scanner.nextInt();
+
+            if (tentative < nombreSecret) {
+                System.out.println("Trop petit !");
+            } else if (tentative > nombreSecret) {
+                System.out.println("Trop grand !");
+            } else {
+                System.out.println("Bravo ! Vous avez trouvé le bon nombre.");
+            }
+        } while (tentative != nombreSecret);
+
+        scanner.close();
     }
-    }
+
 
 
 
